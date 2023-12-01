b44dcf23bf6236db06856c74170d1191ae06dd4b
NhatNguyen103 <nvnhat12b1pdl@gmail.com>
2023-12-01 17:02:41 +0700

    1

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HasRouter} from 'react-router-dom';
+import { HashRouter} from 'react-router-dom';
 import { CartProvider } from 'react-use-cart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HasRouter>
+  <HashRouter>
     <CartProvider>
       <App />
     </CartProvider>
-  </HasRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
